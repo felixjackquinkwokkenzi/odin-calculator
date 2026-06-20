cb6a9d261832bbb1d239906b0081d96adc90c005
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-06-20 12:36:11 +0700

    add core math operations (add, subtract, multiply, divide, operate)

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "cannot divide by 0";
+  }
+
+  return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    return null;
+  }
+
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  } else {
+    return "unknown operator";
+  }
+}
